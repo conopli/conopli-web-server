21798814992ccf0f0eafd26b2905e777315a2b55
sussa3007 <sussa1933@gmail.com>
2023-04-19 14:08:44 +0900

    feat: PlayList 도메인 엔티티 & DTO 작성

---
+++ src/main/java/conopli/webserver/playlist/dto/PlayListDto.java
@@ -0,0 +1,22 @@
+package conopli.webserver.playlist.dto;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class PlayListDto {
+
+    private Long playListId;
+
+    private String title;
+}
