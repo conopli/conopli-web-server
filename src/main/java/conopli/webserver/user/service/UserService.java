320990aae9a5996e9d4189a499a8c032de9da4c5
sussa3007 <sussa1933@gmail.com>
2023-04-21 20:27:20 +0900

    feat: UserService 클래스 생성 및 DTO 수정

--- src/main/java/conopli/webserver/user/service/UserService.java
+++ src/main/java/conopli/webserver/user/service/UserService.java
@@ -1,13 +1,25 @@
 package conopli.webserver.user.service;
 
+import conopli.webserver.user.dto.UserDto;
 import conopli.webserver.user.entity.User;
+import conopli.webserver.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserDto createUser(String email, String nickName, String loginType) {
+
+        return null;
+    }
     public User verifiedUserById(Long userId) {
+
         return null;
     }
 }
