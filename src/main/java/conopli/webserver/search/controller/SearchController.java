e78834d7b93374e6898c0bae81155fa06c2305de
sussa3007 <sussa1933@gmail.com>
2023-04-28 12:08:25 +0900

    refactor: SearchController Stub 응답 작성

--- src/main/java/conopli/webserver/search/controller/SearchController.java
+++ src/main/java/conopli/webserver/search/controller/SearchController.java
@@ -1,10 +1,14 @@
 package conopli.webserver.search.controller;
 
 
-import conopli.webserver.search.dto.PopularDto;
-import conopli.webserver.search.dto.SearchDto;
+import conopli.webserver.dto.PageResponseDto;
+import conopli.webserver.dto.ResponseDto;
+import conopli.webserver.search.dto.*;
+import conopli.webserver.utils.StubUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/search")
@@ -27,23 +33,26 @@ public class SearchController {
             Pageable pageable
     ) {
         // Todo : 음악 검색
-
-        return ResponseEntity.ok().build();
+        List<SearchMusicResponseDto> dtoList = List.of(StubUtils.createSearchMusicDto(), StubUtils.createSearchMusicDto());
+        Page page = new PageImpl(dtoList);
+        PageResponseDto response = PageResponseDto.of(dtoList, page);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/popular")
     public ResponseEntity<?> popularMusic(
-            @ModelAttribute PopularDto requestDto
+            @ModelAttribute PopularRequestDto requestDto
     ) {
         // Todo : 인기곡 검색
-
-        return ResponseEntity.ok().build();
+        List<PopularResponseDto> dtoList = List.of(StubUtils.createPopularResponseDto(), StubUtils.createPopularResponseDto());
+        return ResponseEntity.ok(ResponseDto.of(dtoList));
     }
 
     @GetMapping("/new-music")
     public ResponseEntity<?> newMusic() {
         // Todo : 신곡 검색
-
-        return ResponseEntity.ok().build();
+        List<NewMusicResponseDto> dtoList = List.of(StubUtils.createNewMusicDto(), StubUtils.createNewMusicDto());
+        return ResponseEntity.ok(ResponseDto.of(dtoList));
     }
+
 }
