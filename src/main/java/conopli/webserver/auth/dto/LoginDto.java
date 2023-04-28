9f179c1c19ca28ee1874ac252b59557102455946
sussa3007 <sussa1933@gmail.com>
2023-04-28 12:00:17 +0900

    refactor: LoginDto 빌더 추가

--- src/main/java/conopli/webserver/auth/dto/LoginDto.java
+++ src/main/java/conopli/webserver/auth/dto/LoginDto.java
@@ -1,12 +1,14 @@
 package conopli.webserver.auth.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class LoginDto {
 
     String oauthAccessToken;
