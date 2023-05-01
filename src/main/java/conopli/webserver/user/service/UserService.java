9cd39257ecfe485f0b55ec803b1bf95afe51e28e
sussa3007 <sussa1933@gmail.com>
2023-05-01 16:12:01 +0900

    feat: User Service 로직 일부 구현

--- src/main/java/conopli/webserver/user/service/UserService.java
+++ src/main/java/conopli/webserver/user/service/UserService.java
@@ -1,6 +1,10 @@
 package conopli.webserver.user.service;
 
-import conopli.webserver.user.dto.UserDto;
+import conopli.webserver.auth.token.JwtAuthorityUtils;
+import conopli.webserver.constant.ErrorCode;
+import conopli.webserver.constant.LoginType;
+import conopli.webserver.constant.UserStatus;
+import conopli.webserver.exception.ServiceLogicException;
 import conopli.webserver.user.entity.User;
 import conopli.webserver.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -14,16 +18,35 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserDto createUser(
-            String email,
-            String nickName,
-            String loginType
-    ) {
-
-        return null;
-    }
     public User verifiedUserById(Long userId) {
 
         return null;
+    }
+
+    public User verifiedUserByEmail(String email) {
+        return null;
+    }
+
+    public User createOrVerifiedUserByEmailAndLoginType(String email,String loginType) {
+        try {
+            User findUser = userRepository.findUserByEmail(email);
+            if (findUser.getLoginType().equals(LoginType.valueOf(loginType.toUpperCase()))) {
+                return findUser;
+            } else {
+                throw new ServiceLogicException(ErrorCode.EXIST_USER);
+            }
+        } catch (ServiceLogicException e) {
+            if (e.getErrorCode().equals(ErrorCode.NOT_FOUND_USER)) {
+                User user = User.builder()
+                        .userStatus(UserStatus.VERIFIED)
+                        .email(email)
+                        .loginType(LoginType.valueOf(loginType.toUpperCase()))
+                        .roles(JwtAuthorityUtils.USER_ROLES_STRING_CALL)
+                        .build();
+                return userRepository.saveUser(user);
+            } else {
+                throw e;
+            }
+        }
     }
 }
