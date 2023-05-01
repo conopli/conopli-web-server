0403d97326704af471e1965d048766f763789d45
sussa3007 <sussa1933@gmail.com>
2023-05-01 16:10:49 +0900

    chore: ErrorCode 추가

--- src/main/java/conopli/webserver/constant/ErrorCode.java
+++ src/main/java/conopli/webserver/constant/ErrorCode.java
@@ -6,10 +6,12 @@ public enum ErrorCode {
 
     BAD_REQUEST(400, "BAD REQUEST"),
     EXIST_REQUEST_URL(400, "Already Exist Request Url "),
+    EXIST_USER(400, "Already Exist User Email"),
     ARGUMENT_MISMATCH_BAD_REQUEST(400, "Argument Mismatch Bad Request" ),
     ACCESS_DENIED(403, "ACCESS DENIED"),
     NOT_FOUND(404, "NOT FOUND"),
     NOT_FOUND_REQUEST_URL(404, "Not Found Request Url "),
+    NOT_FOUND_USER(404, "Not Found User"),
     EXPIRED_ACCESS_TOKEN(403, "EXPIRED ACCESS TOKEN"),
     TOKEN_NOT_NULL(404, "TOKEN_NOT_NULL"),
     EXPIRED_REFRESH_TOKEN(401,"EXPIRED REFRESH TOKEN"),
