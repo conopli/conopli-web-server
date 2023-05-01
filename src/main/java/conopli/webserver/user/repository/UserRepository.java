b92f9a8abb084be13ad1cc777822cc82eff0941c
sussa3007 <sussa1933@gmail.com>
2023-05-01 16:11:46 +0900

    feat: User Repository 구현

--- src/main/java/conopli/webserver/user/repository/UserRepository.java
+++ src/main/java/conopli/webserver/user/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package conopli.webserver.user.repository;
 
+import conopli.webserver.constant.LoginType;
+import conopli.webserver.user.entity.User;
+
 public interface UserRepository {
+
+    User saveUser(User user);
+
+    User findUserByEmail(String email);
 }
