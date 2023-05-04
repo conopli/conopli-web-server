589ca04705e40a6ea3b64d6a8a63f9c5294cc440
sussa3007 <sussa1933@gmail.com>
2023-05-04 10:55:13 +0900

    feat: PlayList Repository 구현

--- src/main/java/conopli/webserver/playlist/repository/PlayListJpaRepository.java
+++ src/main/java/conopli/webserver/playlist/repository/PlayListJpaRepository.java
@@ -1,7 +1,12 @@
 package conopli.webserver.playlist.repository;
 
 import conopli.webserver.playlist.entity.PlayList;
+import conopli.webserver.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlayListJpaRepository extends JpaRepository<PlayList, Long> {
+
+    List<PlayList> findAllByUser(User user);
 }
