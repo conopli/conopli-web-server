f5351301f12dc2b0b48375777042aa1d6b4bf8fb
sussa3007 <sussa1933@gmail.com>
2023-05-04 10:55:22 +0900

    feat: UserMusic Repository 구현

--- src/main/java/conopli/webserver/music/repository/UserMusicJpaRepository.java
+++ src/main/java/conopli/webserver/music/repository/UserMusicJpaRepository.java
@@ -1,7 +1,16 @@
 package conopli.webserver.music.repository;
 
 import conopli.webserver.music.entity.UserMusic;
+import conopli.webserver.playlist.entity.PlayList;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserMusicJpaRepository extends JpaRepository<UserMusic, Long> {
+
+    Optional<UserMusic> findUserMusicByNum(String num);
+
+    Page<UserMusic> findUserMusicByPlayList(PlayList playList, Pageable pageable);
 }
