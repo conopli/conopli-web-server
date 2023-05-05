1a3f320dac636bd5adaa72dd6a2efed5b0ae99a5
sussa3007 <sussa1933@gmail.com>
2023-05-05 19:17:50 +0900

    refactor: UserDto Data 어노테이션 추가

--- src/main/java/conopli/webserver/user/dto/UserDto.java
+++ src/main/java/conopli/webserver/user/dto/UserDto.java
@@ -2,12 +2,9 @@ package conopli.webserver.user.dto;
 
 
 import conopli.webserver.user.entity.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Getter
+@Data
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
