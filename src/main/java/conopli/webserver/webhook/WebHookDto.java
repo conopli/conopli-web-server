c136a5d073bcd511d477c262fad6f45afed4183e
sussa3007 <sussa1933@gmail.com>
2023-07-08 17:25:42 +0900

    feat: Discord ErrorLogger WebHook 추가

---
+++ src/main/java/conopli/webserver/webhook/WebHookDto.java
@@ -0,0 +1,14 @@
+package conopli.webserver.webhook;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebHookDto {
+
+    private String content;
+
+}
